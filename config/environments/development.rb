ffd0e5d2c3ee8897f0b720c30c521968a5ce56f6
timdorr <timdorr@timdorr.com>
2013-11-15 23:07:18 -0500

    Pretty print Slim.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ TeslaTrip::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  Slim::Engine.set_default_options pretty: true, sort_attrs: false, format: :html5
 end
