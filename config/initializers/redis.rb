1835d6b81c45aa561589857684129081a3828a12
timdorr <timdorr@timdorr.com>
2013-11-17 12:49:37 -0500

    Add redis caching monkey patch.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,16 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+# Monkey patch in caching for Redis
+class Redis
+  def cache(key, expire=nil, recalculate=false)
+    if (value = get(key)).nil? || recalculate
+      value = yield(self)
+      set(key, value)
+      expire(key, expire) if expire
+      value
+    else
+      value
+    end
+  end
+end
