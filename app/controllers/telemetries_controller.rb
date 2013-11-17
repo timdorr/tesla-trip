f40a6c277bb26d987b2e17d42b200b0e58580932
timdorr <timdorr@timdorr.com>
2013-11-17 13:09:53 -0500

    Add a state endpoint that merges together all data from Tesla's API.

--- app/controllers/telemetries_controller.rb
+++ app/controllers/telemetries_controller.rb
@@ -6,4 +6,14 @@ class TelemetriesController < ApplicationController
     Rails.logger.info(data)
     respond_with data
   end
+
+  def state
+    state = $redis.cache("car-state", 10) do
+      tesla_api = TeslaApi.new(ENV['TESLA_EMAIL'], ENV['TESLA_PASS'])
+      ms = tesla_api.vehicles.first
+      ms.charge_state.merge(ms.drive_state).merge(ms.vehicle_state).merge(ms.climate_state).to_json
+    end
+
+    respond_with JSON.parse(state)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 TeslaTrip::Application.routes.draw do
 
-  resource :telemetry
+  resource :telemetry do
+    get :state
+  end
 
   root "pages#index"
 end
