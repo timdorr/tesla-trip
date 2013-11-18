0e023694af08f16251408114279d1d033594721f
timdorr <timdorr@timdorr.com>
2013-11-17 21:11:14 -0500

    Wrap the redis initializer in a production test to allow asset precompile to work on Heroku.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,16 +1,18 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if ENV["REDISTOGO_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-# Monkey patch in caching for Redis
-class Redis
-  def cache(key, expire=nil, recalculate=false)
-    if (value = get(key)).nil? || recalculate
-      value = yield(self)
-      set(key, value)
-      expire(key, expire) if expire
-      value
-    else
-      value
+  # Monkey patch in caching for Redis
+  class Redis
+    def cache(key, expire=nil, recalculate=false)
+      if (value = get(key)).nil? || recalculate
+        value = yield(self)
+        set(key, value)
+        expire(key, expire) if expire
+        value
+      else
+        value
+      end
     end
   end
 end
