c239b80fc2848a6cb4c7580609cdef302c2438a1
timdorr <timdorr@timdorr.com>
2013-11-18 14:55:33 -0500

    Cache telemetry in Redis. Move JSON parsing into cache.

--- app/controllers/telemetries_controller.rb
+++ app/controllers/telemetries_controller.rb
@@ -2,8 +2,9 @@ class TelemetriesController < ApplicationController
   respond_to :json
 
   def show
-    data = TempodbData.new(Time.parse(ENV["START_TIME"]))
-    Rails.logger.info(data)
+    data = $redis.cache("car-telemetry", 120) do
+      TempodbData.new(Time.parse(ENV["START_TIME"]))
+    end
     respond_with data
   end
 
@@ -11,9 +12,9 @@ class TelemetriesController < ApplicationController
     state = $redis.cache("car-state", 30) do
       tesla_api = TeslaApi.new(ENV['TESLA_EMAIL'], ENV['TESLA_PASS'])
       ms = tesla_api.vehicles.first
-      ms.charge_state.merge(ms.drive_state).merge(ms.climate_state).to_json
+      ms.charge_state.merge(ms.drive_state).merge(ms.climate_state)
     end
 
-    respond_with JSON.parse(state)
+    respond_with state
   end
 end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,17 +1,17 @@
-if ENV["REDISTOGO_URL"]
-  uri = URI.parse(ENV["REDISTOGO_URL"])
+if ENV["REDIS_URL"]
+  uri = URI.parse(ENV["REDIS_URL"])
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
   # Monkey patch in caching for Redis
   class Redis
     def cache(key, expire=nil, recalculate=false)
       if (value = get(key)).nil? || recalculate
-        value = yield(self)
+        value = yield(self).to_json
         set(key, value)
         expire(key, expire) if expire
-        value
+        JSON.parse(value)
       else
-        value
+        JSON.parse(value)
       end
     end
   end
