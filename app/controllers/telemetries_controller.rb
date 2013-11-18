5776402f2fe51c029057ca38ef940f49cf19dd88
timdorr <timdorr@timdorr.com>
2013-11-18 00:18:35 -0500

    Add a box with the current car state.

--- app/controllers/telemetries_controller.rb
+++ app/controllers/telemetries_controller.rb
@@ -8,7 +8,7 @@ class TelemetriesController < ApplicationController
   end
 
   def state
-    state = $redis.cache("car-state", 10) do
+    state = $redis.cache("car-state", 60) do
       tesla_api = TeslaApi.new(ENV['TESLA_EMAIL'], ENV['TESLA_PASS'])
       ms = tesla_api.vehicles.first
       ms.charge_state.merge(ms.drive_state).merge(ms.climate_state).to_json
