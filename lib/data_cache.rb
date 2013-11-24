1b47441bd5d806a3517db66a2dc6a22b481fab50
timdorr <timdorr@timdorr.com>
2013-11-24 02:44:11 -0500

    Don't expire the telemetry.
    What if it fails? Oh bugger, everything shuts off...

--- lib/data_cache.rb
+++ lib/data_cache.rb
@@ -1,15 +1,13 @@
 class DataCache
   def self.telemetry
-    $redis.cache("car-telemetry", 120) do
-      TempodbData.new(Time.parse(ENV["START_TIME"])).to_json
-    end
+    telemetry = TempodbData.new(Time.parse(ENV["START_TIME"])).to_json
+    $redis.set("car-telemetry", telemetry)
   end
 
   def self.state
-    $redis.cache("car-state", 30) do
-      tesla_api = TeslaApi.new(ENV['TESLA_EMAIL'], ENV['TESLA_PASS'])
-      ms = tesla_api.vehicles.first
-      ms.charge_state.merge(ms.drive_state).merge(ms.climate_state).to_json
-    end
+    tesla_api = TeslaApi.new(ENV['TESLA_EMAIL'], ENV['TESLA_PASS'])
+    ms = tesla_api.vehicles.first
+    state = ms.charge_state.merge(ms.drive_state).merge(ms.climate_state).to_json
+    $redis.set("car-state", state)
   end
 end
