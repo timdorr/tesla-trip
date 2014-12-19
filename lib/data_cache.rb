43263733e5f10000070aaef711618b7f0798fa17
Tim Dorr <timdorr@timdorr.com>
2014-12-18 22:28:19 -0500

    Only cache an online state vehicle.

--- lib/data_cache.rb
+++ lib/data_cache.rb
@@ -8,6 +8,7 @@ class DataCache
   def self.state
     tesla_api = TeslaApi::Client.new(ENV["TESLA_EMAIL"], ENV["TESLA_PASS"], ENV["TESLA_CLIENT_ID"], ENV["TESLA_CLIENT_SECRET"])
     ms = tesla_api.vehicles.first
+    return if ms.state != "online"
     state = ms.charge_state.merge(ms.drive_state).merge(ms.climate_state).to_json
     $redis.set("car-state", state)
   end
