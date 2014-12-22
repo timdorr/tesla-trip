d6962f3aea63589c7024d9475fffb96b6dc390f8
Tim Dorr <timdorr@timdorr.com>
2014-12-22 01:17:39 -0500

    Get some debug output for now...

--- lib/data_cache.rb
+++ lib/data_cache.rb
@@ -10,6 +10,7 @@ class DataCache
   end
 
   def self.state
+    TeslaApi::Client.debug_output
     tesla_api = TeslaApi::Client.new(ENV["TESLA_EMAIL"], ENV["TESLA_PASS"], ENV["TESLA_CLIENT_ID"], ENV["TESLA_CLIENT_SECRET"])
     ms = tesla_api.vehicles.first
     return if ms.state != "online"
