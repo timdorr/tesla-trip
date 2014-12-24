ad039ff2f192260b4aef3242ced04e3c860f717e
Tim Dorr <timdorr@timdorr.com>
2014-12-23 22:58:22 -0500

    Receive telemetry for the websocket.

--- app/controllers/telemetries_controller.rb
+++ app/controllers/telemetries_controller.rb
@@ -1,5 +1,7 @@
 class TelemetriesController < ApplicationController
   respond_to :json
+  before_filter :ensure_authentication_token, only: [:create]
+  protect_from_forgery with: :null_session
 
   def show
     data = $redis.get("car-telemetry")
@@ -9,5 +11,29 @@ class TelemetriesController < ApplicationController
   def state
     state = $redis.get("car-state")
     respond_with state
+  end
+
+  def create
+    stream = JSON.parse(request.body.read)
+    $redis.publish(TeslaTrip::TelemetrySocket::CHANNEL, map_stream_to_state(stream).to_json)
+    render json: {status: "ok"}
+  end
+
+  private
+
+  def ensure_authentication_token
+    if params[:api_token] != ENV["WS_TOKEN"]
+      render json: {error: "Bad api token"}, status: :unauthorized
+      return false
+    end
+  end
+
+  def map_stream_to_state(stream)
+    {
+        "latitude" => stream["est_lat"],
+        "longitude" => stream["est_lng"],
+        "speed" => stream["speed"],
+        "battery_level" => stream["soc"]
+    }
   end
 end
