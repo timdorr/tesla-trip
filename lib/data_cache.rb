fbcaa0b6ce5a0fd86f11e7029abe7fe1647586b6
Tim Dorr <timdorr@timdorr.com>
2016-12-14 23:41:49 -0500

    I've got multiple cars now... :D

--- lib/data_cache.rb
+++ lib/data_cache.rb
@@ -1,50 +1,50 @@
 class DataCache
   def self.telemetry
-    telematics = InfluxdbData.new.telematics(Time.parse(ENV["START_TIME"]), Time.parse(ENV["END_TIME"]))
+    telematics = InfluxdbData.new.telematics(Time.parse(ENV['START_TIME']), Time.parse(ENV['END_TIME']))
     return if telematics.blank?
-    telemetry = telematics.map { |t| {lat: t["est_lat"], lng: t["est_lng"]} }
-    $redis.set("car-telemetry", telemetry.to_json)
+    telemetry = telematics.map { |t| {lat: t['est_lat'], lng: t['est_lng']} }
+    $redis.set('car-telemetry', telemetry.to_json)
 
-    car_state = JSON.parse($redis.get("car-state")) || {}
-    $redis.set("car-state", car_state.merge(InfluxdbData.new.latest_state).to_json)
+    car_state = JSON.parse($redis.get('car-state')) || {}
+    $redis.set('car-state', car_state.merge(InfluxdbData.new.latest_state).to_json)
   end
 
   def self.state
     TeslaApi::Client.debug_output
-    tesla_api = TeslaApi::Client.new(ENV["TESLA_EMAIL"], ENV["TESLA_CLIENT_ID"], ENV["TESLA_CLIENT_SECRET"])
+    tesla_api = TeslaApi::Client.new(ENV['TESLA_EMAIL'], ENV['TESLA_CLIENT_ID'], ENV['TESLA_CLIENT_SECRET'])
 
-    token = $redis.cache("tesla-token", 1.hour.to_i) do
-      tesla_api.login!(ENV["TESLA_PASS"])
+    token = $redis.cache('tesla-token', 1.hour.to_i) do
+      tesla_api.login!(ENV['TESLA_PASS'])
       tesla_api.token
     end
     tesla_api.token = token
 
-    vehicle = $redis.cache("tesla-vehicle", 1.hour.to_i) do
-      ms = tesla_api.vehicles.first
+    vehicle = $redis.cache('tesla-vehicle', 1.hour.to_i) do
+      ms = tesla_api.vehicles.find { |v| v.vin == ENV['TESLA_VIN'] }
       ms.vehicle.to_json
     end
     vehicle = JSON.parse(vehicle)
-    ms = TeslaApi::Vehicle.new(tesla_api.class, tesla_api.email, vehicle["id"], vehicle)
+    ms = TeslaApi::Vehicle.new(tesla_api.class, tesla_api.email, vehicle['id'], vehicle)
 
-    puts "getting charge state"
+    puts 'getting charge state'
     charge_state = ms.charge_state rescue nil
     if charge_state.present?
-      car_state = JSON.parse($redis.get("car-state")) || {}
-      $redis.set("car-state", car_state.merge(charge_state).to_json)
+      car_state = JSON.parse($redis.get('car-state')) || {}
+      $redis.set('car-state', car_state.merge(charge_state).to_json)
     end
 
-    puts "getting drive state"
+    puts 'getting drive state'
     drive_state = ms.drive_state rescue nil
     if drive_state.present?
-      car_state = JSON.parse($redis.get("car-state")) || {}
-      $redis.set("car-state", car_state.merge(drive_state).to_json)
+      car_state = JSON.parse($redis.get('car-state')) || {}
+      $redis.set('car-state', car_state.merge(drive_state).to_json)
     end
 
-    puts "getting climate state"
+    puts 'getting climate state'
     climate_state = ms.climate_state rescue nil
     if climate_state.present?
-      car_state = JSON.parse($redis.get("car-state")) || {}
-      $redis.set("car-state", car_state.merge(climate_state).to_json)
+      car_state = JSON.parse($redis.get('car-state')) || {}
+      $redis.set('car-state', car_state.merge(climate_state).to_json)
     end
   end
 end
--- lib/tesla_stream_reader.rb
+++ lib/tesla_stream_reader.rb
@@ -6,19 +6,19 @@ class TeslaStreamReader
   def stream_to_influxdb
     loop do
       loop do
-        puts "Waiting for car to come online..."
-        break if car.state == "online"
+        puts 'Waiting for car to come online...'
+        break if car.state == 'online'
         @car = nil
         sleep 5
       end
 
-      puts "Car online! Streaming car telematics..."
+      puts 'Car online! Streaming car telematics...'
       car.stream do |state|
         puts "Received state: #{state.inspect}"
         write_to_influxdb(state)
         write_to_socket(state)
       end
-      puts "Stream closed!"
+      puts 'Stream closed!'
 
       @tesla_api = nil
       @car = nil
@@ -29,29 +29,29 @@ class TeslaStreamReader
 
   def tesla_api
     @tesla_api ||= begin
-      tesla_api = TeslaApi::Client.new(ENV["TESLA_EMAIL"], ENV["TESLA_CLIENT_ID"], ENV["TESLA_CLIENT_SECRET"])
-      token = $redis.cache("tesla-token", 1.hour.to_i) do
-        puts "Logging into Tesla API..."
-        tesla_api.login!(ENV["TESLA_PASS"])
+      tesla_api = TeslaApi::Client.new(ENV['TESLA_EMAIL'], ENV['TESLA_CLIENT_ID'], ENV['TESLA_CLIENT_SECRET'])
+      token = $redis.cache('tesla-token', 1.hour.to_i) do
+        puts 'Logging into Tesla API...'
+        tesla_api.login!(ENV['TESLA_PASS'])
         tesla_api.token
       end
       tesla_api.token = token
-      puts "Logged in as #{ENV["TESLA_EMAIL"]}!"
+      puts "Logged in as #{ENV['TESLA_EMAIL']}!"
       tesla_api
     end
   end
 
   def car
-    tesla_api.vehicles.first
+    @car ||= tesla_api.vehicles.find { |v| v.vin == ENV['TESLA_VIN'] }
   end
 
   def write_to_influxdb(state)
     state[:time] = state[:time].to_i
-    $influxdb.write_point("telematics", { values: state })
+    $influxdb.write_point('telematics', { values: state })
   end
 
   def write_to_socket(state)
-    HTTParty.post(ENV["TRIP_URL"], body: state.to_json)
+    HTTParty.post(ENV['TRIP_URL'], body: state.to_json)
   rescue
     puts "Websocket error: #{$!}"
   end
