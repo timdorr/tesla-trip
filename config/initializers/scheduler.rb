19cf119393b69755e9851e2aeff28dd017d82234
Tim Dorr <timdorr@timdorr.com>
2016-12-17 21:49:19 -0500

    Rails 5 doesn't autoload in production after booting.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,10 +3,12 @@ scheduler = Rufus::Scheduler::singleton
 
 scheduler.every '1m' do
   Rails.logger.info 'Refresh Telemetry Cache'
+  require_relative '../../lib/data_cache'
   ::DataCache.telemetry
 end
 
 scheduler.every '40s' do
   Rails.logger.info 'Refresh Car State Cache'
+  require_relative '../../lib/data_cache'
   ::DataCache.state
 end
