a4158f5af136b3def160facd9086f13b3759ca83
Tim Dorr <timdorr@timdorr.com>
2016-12-18 22:08:47 -0500

    Only run the scheduler in production mode.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,15 +1,17 @@
-Rails.logger.info 'Starting scheduler...'
-scheduler = Rufus::Scheduler::singleton
+if Rails.env.production?
+  Rails.logger.info 'Starting scheduler...'
+  scheduler = Rufus::Scheduler::singleton
 
-scheduler.every '1m' do
-  Rails.logger.info 'Refresh Telemetry Cache'
-  require_relative '../../lib/data_cache'
-  require_relative '../../lib/influxdb_data'
-  ::DataCache.telemetry
-end
+  scheduler.every '1m' do
+    Rails.logger.info 'Refresh Telemetry Cache'
+    require_relative '../../lib/data_cache'
+    require_relative '../../lib/influxdb_data'
+    ::DataCache.telemetry
+  end
 
-scheduler.every '40s' do
-  Rails.logger.info 'Refresh Car State Cache'
-  require_relative '../../lib/data_cache'
-  ::DataCache.state
+  scheduler.every '40s' do
+    Rails.logger.info 'Refresh Car State Cache'
+    require_relative '../../lib/data_cache'
+    ::DataCache.state
+  end
 end
