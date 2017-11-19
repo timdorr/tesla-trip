fcf5536e7332f2fd9f1812cce0316c59f4410ae5
Tim Dorr <timdorr@timdorr.com>
2017-11-18 23:07:03 -0500

    Make this request SSL now.

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -1,6 +1,6 @@
 $(function() {
 
-$.ajax({ url: 'http://timdorrteslatrip.tumblr.com/api/read/json?num=50', dataType: 'jsonp'})
+$.ajax({ url: 'https://timdorrteslatrip.tumblr.com/api/read/json?num=50', dataType: 'jsonp'})
   .done(function(data){
     $.each(data.posts, function(i, post){
       if (moment(post["date-gmt"], "YYYY-MM-DD H:m:s Z").isBefore('2015-11-21')) return;
