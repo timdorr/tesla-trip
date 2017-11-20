ef9bdfdef82cd9424cc96d3d80408a3fcbe5f780
Tim Dorr <timdorr@timdorr.com>
2017-11-19 22:24:28 -0500

    Remove the social stuff for now.

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -1,11 +1,11 @@
-$(function() {
-
-$.ajax({ url: 'https://timdorrteslatrip.tumblr.com/api/read/json?num=50', dataType: 'jsonp'})
-  .done(function(data){
-    $.each(data.posts, function(i, post){
-      if (moment(post["date-gmt"], "YYYY-MM-DD H:m:s Z").isBefore('2015-11-21')) return;
-      $("#social-container").append(HandlebarsTemplates["tumblr/"+post.type](post));
-    });
-  });
-
-});
+// $(function() {
+//
+// $.ajax({ url: 'https://timdorrteslatrip.tumblr.com/api/read/json?num=50', dataType: 'jsonp'})
+//   .done(function(data){
+//     $.each(data.posts, function(i, post){
+//       if (moment(post["date-gmt"], "YYYY-MM-DD H:m:s Z").isBefore('2015-11-21')) return;
+//       $("#social-container").append(HandlebarsTemplates["tumblr/"+post.type](post));
+//     });
+//   });
+//
+// });
