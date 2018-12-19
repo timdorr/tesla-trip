bef5037e7f9e437ac3f9afbb935eab2bfd3e13a1
Tim Dorr <git@timdorr.com>
2018-12-18 23:30:06 -0500

    Preload the DataCache class.

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -1,14 +1,16 @@
 if Time.parse(ENV['START_TIME']) < Time.now && Time.parse(ENV['END_TIME']) > Time.now
+  require_relative 'data_cache'
+
   Rails.logger.info 'Starting scheduler...'
   scheduler = Rufus::Scheduler::singleton
 
-  scheduler.every '1m' do
+  scheduler.every '1m', first_in: '10s' do
     Rails.logger.info 'Refresh Telemetry Cache'
     require_relative 'influxdb_data'
     ::DataCache.telemetry
   end
 
-  scheduler.every '40s' do
+  scheduler.every '40s', first_in: '20s' do
     Rails.logger.info 'Refresh Car State Cache'
     ::DataCache.state
   end
