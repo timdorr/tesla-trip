0229466fa5975d3c5a2a2ac70fb0cbd1f4236f51
Tim Dorr <git@timdorr.com>
2018-12-20 22:36:01 -0500

    Quicky info box

--- client/app.js
+++ client/app.js
@@ -3,12 +3,20 @@ import { render } from 'react-dom'
 
 import Stories from './components/stories'
 import CarState from './components/car_state'
+import InfoBox from './components/info_box'
 import TeslaMap from './components/map'
 
 render(
   <React.Fragment>
     <Stories />
-    <CarState>{state => <TeslaMap {...state} />}</CarState>
+    <CarState>
+      {state => (
+        <React.Fragment>
+          <InfoBox state={state.state} />
+          <TeslaMap {...state} />
+        </React.Fragment>
+      )}
+    </CarState>
   </React.Fragment>,
   document.getElementById('root')
 )
---
+++ client/components/info_box.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Box } from './styles'
+
+const InfoContainer = styled(Box)`
+  display: grid;
+  grid-template-rows: 35px;
+  grid-template-areas: 'status status';
+  grid-gap: 10px 20px;
+  position: absolute;
+  z-index: 3;
+  right: 20px;
+  bottom: 20px;
+  padding: 15px;
+
+  @media (max-width: 991.98px) {
+    transform: scale(1);
+  }
+`
+
+const InfoStatus = styled.div`
+  grid-area: status;
+  text-align: center;
+  color: #666;
+  font-weight: 600;
+  font-size: 18px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+`
+
+const InfoTitle = styled.div`
+  color: #666;
+  font-size: 14px;
+`
+const InfoData = styled.div``
+
+function toFahrenheit(celsius) {
+  return Math.round(celsius * (9 / 5) + 32)
+}
+
+export default function InfoBox({
+  state: {
+    charging_state,
+    shift_state,
+    speed,
+    charge_rate,
+    time_to_full_charge,
+    usable_battery_level,
+    battery_range,
+    outside_temp,
+    inside_temp
+  }
+}) {
+  return (
+    <InfoContainer>
+      {charging_state == 'Charging' ? (
+        <React.Fragment>
+          <InfoStatus>Charging</InfoStatus>
+
+          <InfoTitle>Charge Rate: </InfoTitle>
+          <InfoData>{Math.round(charge_rate)} mph</InfoData>
+
+          <InfoTitle>Time To Full: </InfoTitle>
+          <InfoData>{time_to_full_charge} hours</InfoData>
+        </React.Fragment>
+      ) : shift_state != 'P' ? (
+        <React.Fragment>
+          <InfoStatus>Driving</InfoStatus>
+
+          <InfoTitle>Speed: </InfoTitle>
+          <InfoData>{speed} mph</InfoData>
+        </React.Fragment>
+      ) : (
+        <InfoStatus>Parked</InfoStatus>
+      )}
+
+      <InfoTitle>Battery Level: </InfoTitle>
+      <InfoData>{Math.round(usable_battery_level)}%</InfoData>
+
+      <InfoTitle>Range: </InfoTitle>
+      <InfoData>{Math.round(battery_range)} mi</InfoData>
+
+      {outside_temp && (
+        <React.Fragment>
+          <InfoTitle>Outside Temp: </InfoTitle>
+          <InfoData>{toFahrenheit(outside_temp)}º</InfoData>
+        </React.Fragment>
+      )}
+
+      {inside_temp && (
+        <React.Fragment>
+          <InfoTitle>Inside Temp: </InfoTitle>
+          <InfoData>{toFahrenheit(inside_temp)}º</InfoData>
+        </React.Fragment>
+      )}
+    </InfoContainer>
+  )
+}
